ecfed57afe9b166849f78e0fed8086cfec3fa16a
Davanon <davanon2014@gmail.com>
2017-06-04 12:40:50 +0530

    First

--- src/generated/java/com/iqmsoft/demo/datatype/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.iqmsoft.com/demo", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.iqmsoft.demo.datatype;
